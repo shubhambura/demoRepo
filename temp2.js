37ff71ff7e4b1a2cc4e492312188ad6e7bc69fbc
shubham bura <shubhambura16@gmail.com>
2024-08-20 12:02:29 +0530

    demo preoject push

---
+++ temp2.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+
+
+function cleanFile(fileName){
+    return new Promise((resolve)=>{
+      let content = fs.readFileSync(fileName,"utf-8")
+      content.trim();
+      fs.writeFileSync(fileName,"Updated file","utf-8")
+      resolve();
+    })
+}
+
+// async function main(){
+//     await cleanFile("a.txt")
+//     console.log("Done Cleaning File")
+// }
+
+function main(){
+    console.log("Done Cleaning File")
+}
+
+cleanFile("a.txt").then(main)
